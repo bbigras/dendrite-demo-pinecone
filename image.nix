e80bb77b049a4f10a5fee0f483037e0ba7d485c8
Bruno Bigras <bigras.bruno@gmail.com>
2021-06-14 16:51:31 -0400

    add docker image

---
+++ image.nix
@@ -0,0 +1,29 @@
+{ pkgs, dendrite }:
+
+let
+  nix-matrix-pinecone = (import ./build.nix { inherit pkgs dendrite; }).main;
+in
+pkgs.dockerTools.buildLayeredImage {
+  name = "nix-matrix-pinecone";
+  created = "now";
+  tag = "latest";
+
+  contents = [
+    nix-matrix-pinecone
+    pkgs.cacert
+  ];
+  config = {
+    Cmd = [
+      "dendrite-demo-pinecone"
+      "-peer"
+      "wss://pinecone.matrix.org/public"
+      "-listen"
+      ":5977"
+    ];
+    ExposedPorts = {
+      "5977/tcp" = { };
+      "5977/udp" = { };
+      "60606/udp" = { };
+    };
+  };
+}
