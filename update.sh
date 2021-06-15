2df6260bf4275cd4ee5a57bcb8bfb79c5046cba9
Bruno Bigras <bigras.bruno@gmail.com>
2021-06-15 16:12:47 -0400

    update: build with nix-build-uncached

--- update.sh
+++ update.sh
@@ -6,7 +6,10 @@ set -euo pipefail
 set -x
 
 nix-env -i nix-prefetch -f '<nixpkgs>'
+nix-env -i nix-build-uncached -f '<nixpkgs>'
 
 vendorSha256="$(nix-instantiate ./packages.nix -A dendrite.vendorSha256 --eval 2>/dev/null | jq -r . || echo "missing_vendorSha256")"
 newvendorSha256="$(nix-prefetch "{ sha256 }: let p=(import ./packages.nix).dendrite; in p.go-modules.overrideAttrs (_: { vendorSha256 = sha256; })")"
 sed -i "s|${vendorSha256}|${newvendorSha256}|" build.nix
+
+nix-build-uncached packages.nix
