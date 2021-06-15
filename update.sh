0ddf5be3283ad55d596ea93bdd60f885e52cd122
Bruno Bigras <bigras.bruno@gmail.com>
2021-06-15 16:00:56 -0400

    push: nix-env -i nix-prefetch

--- update.sh
+++ update.sh
@@ -2,6 +2,8 @@
 
 # based on https://github.com/colemickens/nixpkgs-wayland/blob/c9c4a80715557caf57f403038a267f3c20859424/update.sh
 
+nix-env -i nix-prefetch
+
 vendorSha256="$(nix-instantiate ./packages.nix -A dendrite.vendorSha256 --eval 2>/dev/null | jq -r . || echo "missing_vendorSha256")"
 newvendorSha256="$(nix-prefetch "{ sha256 }: let p=(import ./packages.nix).dendrite; in p.go-modules.overrideAttrs (_: { vendorSha256 = sha256; })")"
 sed -i "s|${vendorSha256}|${newvendorSha256}|" build.nix
