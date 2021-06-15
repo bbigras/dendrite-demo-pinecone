2dc1383b968163e10c594565acc59cf3e97fad6b
Bruno Bigras <bigras.bruno@gmail.com>
2021-06-15 16:09:22 -0400

    add nixpkgs

--- update.sh
+++ update.sh
@@ -5,7 +5,7 @@
 set -euo pipefail
 set -x
 
-nix-env -i nix-prefetch
+nix-env -i nix-prefetch -f '<nixpkgs>'
 
 vendorSha256="$(nix-instantiate ./packages.nix -A dendrite.vendorSha256 --eval 2>/dev/null | jq -r . || echo "missing_vendorSha256")"
 newvendorSha256="$(nix-prefetch "{ sha256 }: let p=(import ./packages.nix).dendrite; in p.go-modules.overrideAttrs (_: { vendorSha256 = sha256; })")"
