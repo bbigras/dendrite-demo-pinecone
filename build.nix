4fb31a3164f3e77b3fec3d7a44d2d7b3cd251e7b
Bruno Bigras <bigras.bruno@gmail.com>
2021-06-25 10:14:12 -0400

    flake.lock: Update
    Flake input changes:
    
    * Updated 'dendrite': 'github:matrix-org/dendrite/097a5833c883ddf3fac10fa2331ca644b98bc100' -> 'github:matrix-org/dendrite/bd7b0b1787363535972bed719004297127535479'

--- build.nix
+++ build.nix
@@ -7,7 +7,7 @@ with pkgs;
 
     src = dendrite;
 
-    vendorSha256 = "sha256-eqo7bm4KLhnTL9U1ZVB6MXn9XYGuucW7oj1CGuVCcdE=";
+    vendorSha256 = "sha256-6FmWBsUJv/1hnT94uQIZAoLzl2HIcURafGE7xFdyLAc=";
     subPackages = [ "cmd/dendrite-demo-pinecone" ];
   };
 
