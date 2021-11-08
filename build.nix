8f06fbc1942cd932248b0bb23f9de923521929ac
bbigras <bbigras@users.noreply.github.com>
2021-11-08 12:31:31 +0000

    flake: update

--- build.nix
+++ build.nix
@@ -7,7 +7,7 @@ with pkgs;
 
     src = dendrite;
 
-    vendorSha256 = "sha256-AVRdJxWK0jwTS9vR+OatUnMjJCORezEaHE5wevmdXV8=";
+    vendorSha256 = "sha256-XCj/8uFow+zN1/99eQUTL4F7jCjWESlb1WuTJpJl8i4=";
     subPackages = [ "cmd/dendrite-demo-pinecone" ];
   };
 
