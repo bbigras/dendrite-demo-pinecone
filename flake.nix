38e31abe27c320422d679cfef910ef4087b021d8
Bruno Bigras <bigras.bruno@gmail.com>
2021-11-30 14:32:19 -0500

    + pre-commit-hooks

--- flake.nix
+++ flake.nix
@@ -13,9 +13,16 @@
       url = "github:edolstra/flake-compat";
       flake = false;
     };
+    pre-commit-hooks = {
+      url = "github:cachix/pre-commit-hooks.nix";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        flake-utils.follows = "flake-utils";
+      };
+    };
   };
 
-  outputs = { self, nixpkgs, flake-utils, dendrite, flake-compat }:
+  outputs = { nixpkgs, flake-utils, dendrite, pre-commit-hooks, ... }:
     flake-utils.lib.eachDefaultSystem
       (system:
         let pkgs = import nixpkgs { inherit system; }; in
@@ -23,7 +30,7 @@
           defaultPackage = (import ./build.nix { inherit pkgs dendrite; }).main;
           packages.dendrite = (import ./build.nix { inherit pkgs dendrite; }).main;
           packages.dockerImage = import ./image.nix { inherit pkgs dendrite; };
-          devShell = import ./shell.nix { inherit pkgs; };
+          devShell = import ./shell.nix { inherit pkgs pre-commit-hooks system; };
         }
       );
 }
--- shell.nix
+++ shell.nix
@@ -1,8 +1,26 @@
-{ pkgs }:
+{ pkgs, pre-commit-hooks, system }:
 
+let
+  pre-commit-check = pre-commit-hooks.lib.${system}.run {
+    src = ./.;
+    hooks = {
+      nixpkgs-fmt.enable = true;
+      nix-linter.enable = true;
+      shellcheck.enable = true;
+    };
+    # generated files
+    excludes = [
+      "Cargo.nix"
+    ];
+  };
+in
 pkgs.mkShell {
   buildInputs = [
     # keep this line if you use bash
     pkgs.bashInteractive
   ];
+
+  shellHook = ''
+    ${pre-commit-check.shellHook}
+  '';
 }
