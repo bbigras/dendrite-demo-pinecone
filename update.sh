c68661f8d2beeed0417576a1283b37c3818873d7
Bruno Bigras <bigras.bruno@gmail.com>
2024-01-19 22:29:14 -0500

    try to fix update.sh

--- update.sh
+++ update.sh
@@ -6,5 +6,14 @@ set -euo pipefail
 set -x
 
 vendorHash="$(nix-instantiate ./packages.nix -A dendrite.vendorHash --eval 2>/dev/null | jq -r . || echo "missing_vendorHash")"
-newvendorHash="$(nix run nixpkgs#nix-prefetch "{ sha256 }: let p=(import ./packages.nix).dendrite; in p.goModules.overrideAttrs (_: { vendorHash = sha256; })")"
-sed -i "s|${vendorHash}|${newvendorHash}|" build.nix
+
+sed -i "s|${vendorHash}|sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=|" build.nix
+run_build="$(nix build 2>&1 || true)"
+
+if [[ $run_build == *"hash mismatch in fixed-output derivation"* ]]; then
+	regex="got:[ ]+(sha256[^=]+=)"
+
+	if [[ $run_build =~ $regex ]]; then
+		sed -i "s|sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=|${BASH_REMATCH[1]}|" build.nix
+	fi
+fi
